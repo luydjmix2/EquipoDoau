3453b6502bdb9bac8004e68ce1c47a5725b7adf4
YosindiOrtega <practicante03@nes360.org>
2020-09-25 17:24:25 -0500

    inde

---
+++ resources/views/index.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html class="" xmlns:fb="" xmlns:og="" lang="en">
+<head>
+    <script type="" defer="" async="" src="" ></script>
+
+    <link rel="stylesheet" href=""> <! CSS >
+    
+    <meta http-equiv="Content-Type" content="text/html">
+    <meta charset="UTF-8">     
+    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Tus opciones Delivery en tu Barrio con Doau Domicilio">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Doau en tu Barrio</title>
+</head>
+<body>
+    <h1 class="text-center">¿Dónde nos necesitas?</h1>
+    
+    <section>
+        <div>
+            <span class="location-text location-text-address">Dirección de entrega</span>
+            <form action="" method="POST" >
+                <select class="custom-select"><option value="972">Armenia</option>
+                    <option value="996">Barrancabermeja</option>
+                    <option value="1009">Barranquilla</option>
+                    <option value="1086">Bello</option>
+                    <option value="975" selected="selected">Bogotá</option>
+                    <option value="1013">Bucaramanga</option>
+                    <option value="991">Cali</option>
+                    <option value="1126">Cañaveral</option>
+                    <option value="1005">Cartagena</option>
+                    <option value="982">Cartago</option>
+                    <option value="1742">Caucasia</option>
+                    <option value="979">Chia</option>
+                    <option value="1745">Cota</option>
+                    <option value="974">Cucuta</option>
+                    <option value="1740">Dos Quebradas</option>
+                    <option value="1101">Dosquebradas</option>
+                    <option value="1737">El tesoro</option>
+                    <option value="1089">Envigado</option>
+                    <option value="1122">Floridablanca</option>
+                    <option value="983">Funza</option>
+                    <option value="1463">Gaira/Rodadero</option>
+                    <option value="1008">Girardot</option>
+                    <option value="1123">Girón</option>
+                    <option value="981">Ibague</option>
+                    <option value="1087">Itagüi</option>
+                    <option value="1072">Jamundí</option>
+                    <option value="1000">La Calera</option>
+                    <option value="1739">Madrid</option>
+                    <option value="987">Manizales</option>
+                    <option value="980">Medellín</option>
+                    <option value="1007">Monteria</option>
+                    <option value="973">Mosquera</option>
+                    <option value="985">Neiva</option>
+                    <option value="1016">Palmira</option>
+                    <option value="1002">Pasto</option>
+                    <option value="1011">Pereira</option>
+                    <option value="1124">Piedecuesta</option>
+                    <option value="1103">Poblado</option>
+                    <option value="1003">Popayán</option>
+                    <option value="1743">Riohacha</option>
+                    <option value="1426">Rionegro</option>
+                    <option value="1088">Sabaneta</option>
+                    <option value="978">Santa Marta</option>
+                    <option value="998">Santa Rosa de Cabal</option>
+                    <option value="1744">Sincelejo</option>
+                    <option value="977">Soacha</option>
+                    <option value="986">Soledad</option>
+                    <option value="1104">Tesoro</option>
+                    <option value="1462">Tocancipá</option>
+                    <option value="1741">Tulua</option>
+                    <option value="1014">Tunja</option>
+                    <option value="997">Valledupar</option>
+                    <option value="984">Villavicencio</option>
+                    <option value="1077">Yumbo</option>
+                </select>
+                <select class="custom-select custom-select-lg mb-3">
+                <option selected>Open this select menu</option>
+                <option value="1">Calle</option>
+                <option value="2">Carrera</option>
+                <option value="3">Avenida</option>
+                <option value="4">Avenida Carrera</option>
+                <option value="5">Avenida Calle</option>
+                <option value="6">Circular</option>
+                <option value="7">Circunvalar</option>
+                <option value="8">Diagonal</option>
+                <option value="9">Manzana</option>
+                <option value="10">Transversal</option>
+                <option value="11">Vía</option>   
+                </select>
+                
+                <input type="text" placeholder="Digite  su Dirección" required="">
+                </input>
+                <input type="text" placeholder="#" required="">
+                </input>
+                <input type="text" placeholder="Digite  su Dirección" required="">
+                </input>
+                <button onclick="" type="button" >Buscar</button>
+            </form>
+        </div>
+    </section>
+    <section>
+        <br>
+        <div>Contenedor</div>
+        <br>
+    </section>
+    <footer class="content_module new_row new_footer show">
+        <section class="new_center_content" >
+            <div class="row content_social_apps"> 
+                <ul class="content_links">
+                    <li><a href="/about">Nosotros</a></li>
+                    <li><a href="/about/politicas-de-privacidad">Privacidad</a></li>
+                    <li><a href="/about/terminos-condiciones">Condiciones</a></li>
+                     <li><a href="http://www.sic.gov.co/" target="_blank">Secretaría de Industria y Comercio</a></li>    
+                </ul>
+                
+                <ul class="content_links">
+                    <li><a href="https://deliveryhero.wd3.myworkdayjobs.com/domicilios" target="_blank" rel="noopener noreferrer">Sé un DomiHéroe</a></li>
+                    <li><button id="suggestRestoBtn" class="button suggest" rel="nofollow">Registra tu negocio</button></li>   
+                </ul>   
+               <!--Social-->
+                <ul class="content_social">
+                    <li class="title">Síguenos en</li>
+                    <li class="fb"><a href="https://www.facebook.com" target="_blank" class="facebook" title="Facebook">Facebook</a></li>
+                    <li class="tw"><a href="https://twitter.com" target="_blank" class="twitter" title="Twitter">Twitter</a></li>
+                    <li class="yt"><a href="https://www.youtube.com" target="_blank" class="youtube" title="Youtube">Youtube</a> </li>
+                </ul>
+                <!--Apps-->
+                 <ul class="content_apps">                      <li class="title">Descargar app</li>
+                    <li class="ic_android">           <a href="https://play.google.com/store&amp;referrer=none" target="_blank" class="android" title="Android">Android</a>
+                     </li>
+                </ul>
+            </div>
+            <hr class="divider">
+            <p class="text-center">Copyright - 2020 - Doau</p>
+        </section>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/6b0ce0e597.js" crossorigin="anonymous"></script>
+</body>
+</html>
